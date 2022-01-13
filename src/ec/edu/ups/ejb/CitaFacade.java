9bf7e3f8b7eac82f40bbdc4af4df2a5301bd82c9
Andres Bermeo <andipandi467@gmail.com>
2022-01-13 04:22:00 -0500

    Issues Completed with the JQPL in the PacienteResource

--- src/ec/edu/ups/ejb/CitaFacade.java
+++ src/ec/edu/ups/ejb/CitaFacade.java
@@ -1,10 +1,15 @@
 package ec.edu.ups.ejb;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import ec.edu.ups.model.Cita;
+import ec.edu.ups.model.Persona;
 
 @Stateless
 public class CitaFacade extends AbstractFacade<Cita> {
@@ -20,6 +25,42 @@ public class CitaFacade extends AbstractFacade<Cita> {
 	protected EntityManager getEntityManager() {
 		return entityManager;
 	}
+	
+	public List<Cita> getCitasbyEstado(String estado) {
+		String query = "SELECT c FROM Cita c WHERE c.estadoCita = :estado";
+		List<Cita> citas = new ArrayList<Cita>();
+		
+		try {
+			citas = (List<Cita>) entityManager.createQuery(query)
+											 .setParameter("estado", estado)
+											 .getResultList();
+			
+			
+		} catch (Exception e) {
+			System.out.println("--> ERROR Cita.getCitasbyEstado" + e.getMessage());
+		}
+		return citas;
+	}
+	
+	public List<Cita> getCitasbyDate(Date fecha) {
+		String query = "SELECT c FROM Cita c WHERE c.fechaCita = :fecha";
+		List<Cita> citas = new ArrayList<Cita>();
+		
+		try {
+			citas = (List<Cita>) entityManager.createQuery(query)
+											 .setParameter("fecha", fecha)
+											 .getResultList();
+			
+			System.out.println("CItas: " + citas);
+			
+			
+		} catch (Exception e) {
+			System.out.println("--> ERROR Cita.getCitasbyEstado" + e.getMessage());
+		}
+		return citas;
+	}
+	
+	
 	
 	
 
